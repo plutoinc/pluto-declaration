f90fcde02575e94c76cfb5ade82884a3efcef29d
academey <academey@gmail.com>
2017-11-11 19:16:14 +0900

    Add SignBox Styling

--- app/components/home/components/signBox/index.tsx
+++ app/components/home/components/signBox/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { withStyles } from "../../../../helpers/withStylesHelper";
+import Icon from "../../../../icons/index";
 const styles = require("./signBox.scss");
 
 interface ISignBoxComponentProps {
@@ -18,7 +19,29 @@ export default class SignBanner extends React.PureComponent<ISignBoxComponentPro
           // position: isBoxMovingHeight ? "static" : "fixed",
         }}
       >
-        test
+        <div className={styles.title}>Add your name to the list!</div>
+        <div className={styles.inputWrapper}>
+          <Icon className={styles.iconWrapper} icon="TWITTER" />
+          <input className={`form-control ${styles.inputBox}`} placeholder="Name" />
+        </div>
+        <div className={styles.inputWrapper}>
+          <Icon className={styles.iconWrapper} icon="TWITTER" />
+          <input className={`form-control ${styles.inputBox}`} placeholder="Affiliation" />
+        </div>
+        <div className={styles.inputWrapper}>
+          <Icon className={styles.iconWrapper} icon="TWITTER" />
+          <input className={`form-control ${styles.inputBox}`} placeholder="Affiliation E-mail" />
+        </div>
+        <div className={styles.caution}>{`* Used to verify identity. It will not be shared or
+        displayed.`}</div>
+        <div className={styles.commentInputWrapper}>
+          <textarea className={`form-control ${styles.inputBox}`} placeholder="Additional comment (Option)" />
+        </div>
+        <div className={styles.checkBoxContainer}>
+          <div className={styles.checkBox} />
+          <span className={styles.checkBoxContent}>Send me email updates about the project (Option)</span>
+        </div>
+        <div className={styles.submitButton}>Sign</div>
       </div>
     );
   }
